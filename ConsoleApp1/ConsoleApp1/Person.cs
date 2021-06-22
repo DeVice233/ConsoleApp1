1d513a86506ad49edf0ca1b332c479fea47de032
DeVice <yakovenko182003@gmail.com>
2021-06-22 14:30:50 +1000

    в

---
+++ ConsoleApp1/ConsoleApp1/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Person
+    {
+        public string Name;
+        public int Age;
+        public int Weight;
+
+
+        public string GetValue()
+        {
+            string value;
+            value = "Имя:" + this.Name + "  Возраст:" + this.Age + "  Вес:" + this.Weight + "\n";
+            return value;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -7,10 +8,30 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(File.ReadAllText("D:\\TestFile.txt"));
+            List<Person> people = new List<Person>();
+            string name;
+            int age;
+            int weight;
+            Console.WriteLine("Введите имя");
+            name = Console.ReadLine();
+            Console.WriteLine("Введите возраст");
+            age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите вес");
+            weight = Convert.ToInt32(Console.ReadLine());
+            Person person1 = new Person { Age = age, Name = name, Weight = weight };
+            people.Add(person1);
+            
 
-           
-
+            File.AppendAllText("D:\\TestFile.txt", "текст");
+            File.WriteAllText("D:\\TestFile.txt", person1.GetValue());
+            Console.WriteLine(File.ReadAllText("D:\\TestFile.txt"));
         }
+
+
+
+
+
 
         public void Stack() //про свою реализацию стека хотя не уверен это наверное мало???
         {
